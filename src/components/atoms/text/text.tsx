894fc9b5d8dd46b5731c71ee5250c9b8381d2420
Bruno-Marcon <bruno.marconcco@gmail.com>
2025-05-06 22:18:11 -0300

    create atom text

---
+++ src/components/atoms/text/text.tsx
@@ -0,0 +1,30 @@
+import { Text } from 'react-native';
+
+type TextProps = React.ComponentProps<typeof Text> & {
+  variant?: 'primary' | 'secondary' | 'button';
+  size?: 'xl' | 'lg' | 'base' | 'sm';
+};
+
+const variantClasses = {
+  primary: 'text-gray-800',
+  secondary: 'text-gray-500',
+  button: 'text-white font-medium',
+};
+
+const sizeClasses = {
+  xl: 'text-2xl',
+  lg: 'text-lg',
+  base: 'text-base',
+  sm: 'text-sm',
+};
+
+export const CustomText = ({ 
+  variant = 'primary', 
+  size = 'base', 
+  className = '', 
+  ...props 
+}: TextProps) => {
+  const classes = `${variantClasses[variant]} ${sizeClasses[size]} ${className}`;
+  
+  return <Text className={classes} {...props} />;
+};
