52ee18164328f299cb82ac948ddc0d0795c53d40
Bruno-Marcon <bruno.marconcco@gmail.com>
2025-05-06 22:16:39 -0300

    update occrrence card

--- src/components/atoms/card/OccurrenceCard.tsx
+++ src/components/atoms/card/OccurrenceCard.tsx
@@ -1,19 +1,31 @@
-import { View, Text } from 'react-native';
-import { Feather } from '@expo/vector-icons';  // Importando ícones do Expo
+import React from "react";
+import { View, Text, ActivityIndicator, TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { useOccurrence } from "@//hook/useOccurence";
+import { formatDate } from "@//utils/dateUtils";
 
-interface OccurrenceCardProps {
-  title: string;
-  description: string;
-}
 
-export default function OccurrenceCard({ title, description }: OccurrenceCardProps) {
+export default function OccurrenceCard() {
+  const { occurrences, loading, error } = useOccurrence();
+
+  if (loading) return <ActivityIndicator size="large" color="#3498db" />;
+  if (error) return <Text className="text-center text-red-500">{error}</Text>;
+  if (!occurrences.length) return <Text className="text-center">Nenhuma ocorrência disponível.</Text>;
+
   return (
-    <View className="flex-row items-start space-x-3 mb-4 bg-white p-4 rounded-xl shadow-sm">
-      <Feather name="alert-circle" size={24} color="#DC2626" /> {/* Usando o ícone 'alert-circle' */}
-      <View className="flex-1">
-        <Text className="text-base font-semibold text-gray-700">{title}</Text>
-        <Text className="text-sm text-gray-500">{description}</Text>
-      </View>
+    <View>
+      {occurrences.map((occurrence, index) => (
+        <TouchableOpacity key={index} className="p-2 rounded-xl shadow-lg mb-3 bg-white">
+          <View className="ml-2 flex-row items-center gap-x-4">
+            <Feather name="alert-circle" size={28} color="#DC2626" />
+            <View className="flex-1">
+              <Text className="text-lg font-semibold text-gray-800">{occurrence.title}</Text>
+              <Text className="text-base text-gray-600 mt-1">{occurrence.description}</Text>
+              <Text className="text-xs text-gray-500 mt-1">{formatDate(occurrence.createdAt)}</Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+      ))}
     </View>
   );
 }
