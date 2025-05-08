47551f438919b484eadf8cd165ff7aeedefb08cb
Bruno Marcon <55155016+Bruno-Marcon@users.noreply.github.com>
2025-05-08 08:54:21 -0300

    Adjust size avatar

--- src/components/organisms/navbar/defaultNav.tsx
+++ src/components/organisms/navbar/defaultNav.tsx
@@ -13,7 +13,7 @@ type DefaultNavBarProps = {
 const DefaultNavBar: React.FC<DefaultNavBarProps> = ({
   logo = <LogoIFC className="w-14 h-14" />,
   icons = [<DynamicIcon name="bell" size={24} color="#4B5563" />],
-  avatar = <ProfileAvatar />,
+  avatar = <ProfileAvatar size={40} modal={true}/>,
 }) => {
   return (
     <View className="flex-row items-center gap-x-4 justify-between px-4 py-4">
